941e9d75e41e9013e0638ff3b7f890f67f2484f6
Lautaro Casanueva <casanuevalautaro@gmail.com>
2022-05-09 21:07:05 -0300

    Beta 1.0 terminada

--- src/main/java/com/example/Eventbring/controladores/ListController.java
+++ src/main/java/com/example/Eventbring/controladores/ListController.java
@@ -5,10 +5,13 @@
  */
 package com.example.Eventbring.controladores;
 
+import com.example.Eventbring.entidades.Asistencia;
 import com.example.Eventbring.entidades.Evento;
 import com.example.Eventbring.entidades.Usuario;
+import com.example.Eventbring.repositorios.EventoRepositorio;
 import com.example.Eventbring.servicios.AsistenciaServicio;
 import com.example.Eventbring.servicios.EventoServicio;
+import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -30,17 +33,22 @@ public class ListController {
     @Autowired
     private EventoServicio es;
     
+    @Autowired
+    private EventoRepositorio er;
+    
     @GetMapping("/asistir/{id}")
     public String asistir(@PathVariable String id, HttpSession session) {
 
         try {
             
             Usuario u = (Usuario) session.getAttribute("usuariosession");
-            
-            
+                       
             as.registrar(u.getId(), id);
             
+
             es.reducirCupo(id);
+            
+            
             
             return "redirect:/listaeventos";
         } catch (Exception e) {
@@ -53,6 +61,15 @@ public class ListController {
     public String borrar(@PathVariable String id) {
 
         try {
+            
+            List<Asistencia> asistencias = as.listarTodos();
+            
+            for (Asistencia asistencia : asistencias) {
+                if(asistencia.getEventos().equals(id)){
+                    as.borrar(asistencia.getId());
+                }
+                
+            }
             
             es.borrar(id);
             
